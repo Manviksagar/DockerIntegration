d119777fe27a1bb5a539867ccd12da25185af086
Manviksagar <aallu.vidyasagar@gmail.com>
2020-01-15 08:44:36 +0530

    Secong commit

---
+++ src/main/java/com/sag/docker/Hello.java
@@ -0,0 +1,11 @@
+package com.sag.docker;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sagar working on Git docker integration");
+
+	}
+
+}
